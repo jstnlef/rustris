076497d0e8e2eed1539c063744464c8ccab1d2b1
Justin LeFebvre <jstnlefebvre@gmail.com>
2016-01-14 19:15:24 -0500

    Got a red square at the top of the screen! Figured out how to get cargo to build the rust code appropriately. Hurray!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate piston_window;
+
+use piston_window::*;
+
 fn main() {
-    println!("Hello, world!");
+    let window: PistonWindow = WindowSettings::new("Rustris", [640, 480])
+                               .exit_on_esc(true).build().unwrap();
+    for e in window {
+        e.draw_2d(|c, g| {
+            clear([1.0; 4], g);
+            rectangle([1.0, 0.0, 0.0, 1.0], // red
+                      [0.0, 0.0, 100.0, 100.0],
+                      c.transform, g);
+        });
+    }
 }
