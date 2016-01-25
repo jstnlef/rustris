28a9c7c0763c9415b52e73096b505a69ea49bf69
Justin LeFebvre <jstnlefebvre@gmail.com>
2016-01-25 01:30:41 -0500

    add the cargo version number to the title string

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,14 @@ extern crate piston_window;
 
 use piston_window::*;
 
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+const WINDOW_WIDTH: u32 = 640;
+const WINDOW_HEIGHT: u32 = 480;
+
 fn main() {
-    let window: PistonWindow = WindowSettings::new("Rustris", [640, 480])
-                               .exit_on_esc(true).build().unwrap();
+    let window_title = format!("Rustris {}", VERSION);
+    let window: PistonWindow = WindowSettings::new(window_title, [WINDOW_WIDTH, WINDOW_HEIGHT])
+        .exit_on_esc(true).build().unwrap();
     for e in window {
         e.draw_2d(|c, g| {
             clear([1.0; 4], g);
