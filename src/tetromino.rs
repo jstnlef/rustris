91a33173a758c0df1ff067937054e6993b2ab326
Justin LeFebvre <jstnlefebvre@gmail.com>
2016-02-13 21:38:05 -0500

    added a test for rotate as well as started the transition to dealing with Position structs rather than x and y separately

--- src/tetromino.rs
+++ src/tetromino.rs
@@ -1,5 +1,6 @@
 use conrod::{color, Color};
 
+// TODO: look into having a make-block! macro for when this inevitably needs to change
 pub const I: Tetromino = Tetromino {
     configurations: [
         [Block{x:0, y:2, color: CYAN}, Block{x:1, y:2, color: CYAN}, Block{x:2, y:2, color: CYAN}, Block{x:3, y:2, color: CYAN}],
@@ -58,21 +59,30 @@ pub const Z: Tetromino = Tetromino {
 };
 
 // Tetromino colors
-const CYAN: Color = Color::Rgba(0x00 as f32, 0xFF as f32, 0xff as f32, 1.0);
-const BLUE: Color = color::BLUE;
-const ORANGE: Color = color::ORANGE;
-const YELLOW: Color = color::YELLOW;
-const LIME: Color = Color::Rgba(0x80 as f32, 0xFF as f32, 0x00 as f32, 1.0);
-const PURPLE: Color = color::PURPLE;
-const RED: Color = color::RED;
+pub const CYAN: Color = Color::Rgba(0x00 as f32, 0xFF as f32, 0xff as f32, 1.0);
+pub const BLUE: Color = color::BLUE;
+pub const ORANGE: Color = color::ORANGE;
+pub const YELLOW: Color = color::YELLOW;
+pub const LIME: Color = Color::Rgba(0x80 as f32, 0xFF as f32, 0x00 as f32, 1.0);
+pub const PURPLE: Color = color::PURPLE;
+pub const RED: Color = color::RED;
 
 pub struct Piece {
-    x: u32,
-    y: u32,
+    // TODO: Replace with position
+    x: u8,
+    y: u8,
     ptype: Tetromino,
     rotation: Rotation
 }
 impl Piece {
+    fn new(position: Position, ptype: Tetromino, rotation: Rotation) -> Piece {
+        Piece {
+            x: position.x,
+            y: position.y,
+            ptype: ptype,
+            rotation: rotation
+        }
+    }
     fn get_blocks(&self) -> &Configuration {
         self.ptype.get_configuration(&self.rotation)
     }
@@ -87,7 +97,8 @@ impl Piece {
     }
 }
 
-enum Rotation {
+#[derive(Debug, PartialEq)]
+pub enum Rotation {
     R0, R1, R2, R3
 }
 
@@ -110,9 +121,33 @@ impl Tetromino {
 type Configuration = [Block; 4];
 
 // Store the x and y position relative to local coordinates
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct Block {
     x: u8,
     y: u8,
     color: Color
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Position {
+    x: u8,
+    y: u8
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_piece_rotation() {
+        let position = Position{x:0, y:0};
+        let mut p = Piece::new(position, I, Rotation::R0);
+        p.rotate();
+	    assert_eq!(p.rotation, Rotation::R1);
+        p.rotate();
+	    assert_eq!(p.rotation, Rotation::R2);
+        p.rotate();
+	    assert_eq!(p.rotation, Rotation::R3);
+        p.rotate();
+	    assert_eq!(p.rotation, Rotation::R0);
+    }
 }
