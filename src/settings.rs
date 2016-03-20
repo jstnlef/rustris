0bc2868d95ed4d33747507ff2a3dd53d9ae0c818
Justin LeFebvre <jstnlefebvre@gmail.com>
2016-03-20 19:25:38 -0400

    messing around with some of the settings

--- src/settings.rs
+++ src/settings.rs
@@ -1,9 +1,11 @@
 pub const VERSION: &'static str = env!("CARGO_PKG_VERSION");
-pub const WINDOW_WIDTH: u32 = 400;
+pub const WINDOW_WIDTH: u32 = 800;
 pub const WINDOW_HEIGHT: u32 = 600;
-pub const HEIGHT_IN_BLOCKS: i32 = 22;
+pub const HEIGHT_IN_BLOCKS: i32 = 20;
 pub const WIDTH_IN_BLOCKS: i32 = 10;
-pub const BLOCK_SIZE: f64 = 25.0;
+pub const BLOCK_SIZE: f64 = 27.0;
 pub const GRID_LINE_WIDTH: f64 = 1.0;
-pub const GRID_X_OFFSET: f64 = 10.0;
-pub const GRID_Y_OFFSET: f64 = 10.0;
+pub const GRID_X_OFFSET: f64 = (
+    WINDOW_WIDTH as f64 / 2.0 - (WIDTH_IN_BLOCKS as f64/2.0 * BLOCK_SIZE)
+);
+pub const GRID_Y_OFFSET: f64 = 25.0;
